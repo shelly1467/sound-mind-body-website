03a53d10e241ab1fe769fcdb2acb6a41007a62bc
Shelly Gaines <shellygaines@gmail.com>
2026-05-11 17:11:18 -0700

    Update video script

--- script.js
+++ script.js
@@ -1,1 +1,39 @@
-console.log('Sound Mind & Body website is working.');
+const videos = [
+  {
+    title: "Featured Affirmation Video",
+    id: "YOUR_VIDEO_ID"
+  },
+  {
+    title: "Affirmation Video 2",
+    id: "YOUR_VIDEO_ID"
+  },
+  {
+    title: "Affirmation Video 3",
+    id: "YOUR_VIDEO_ID"
+  }
+];
+
+function createVideo(video) {
+  return 
+    <div class="video-card">
+      <iframe
+        src="https://www.youtube.com/embed/"
+        title=""
+        frameborder="0"
+        allowfullscreen>
+      </iframe>
+      <h3></h3>
+    </div>
+  ;
+}
+
+const featuredVideo = document.querySelector("#featured-video");
+const videoGallery = document.querySelector("#video-gallery");
+
+if (featuredVideo) {
+  featuredVideo.innerHTML = createVideo(videos[0]);
+}
+
+if (videoGallery) {
+  videoGallery.innerHTML = videos.slice(1).map(createVideo).join("");
+}
